775d139348069de3b0c2adabb81c5ca2d6fc1a5c
Spoorthi R <spoorthir740@gmail.com>
2025-11-28 13:19:23 +0530

    Delete GoshoppingApp/src/org directory

--- GoshoppingApp/src/org/framework/ShopFactory.java
+++
@@ -1,9 +0,0 @@
-package org.framework;
-//abstract class
-public abstract class ShopFactory {
-	public abstract PrimeAcc getNewPrimeAccount(int accNo, String accNm, float charges, boolean isPrime);
-	public abstract NormalAcc getNewNormalAccount(int accNo, String accNm, float charges, float deliveryCharges);
-	
-	
-     
-}
